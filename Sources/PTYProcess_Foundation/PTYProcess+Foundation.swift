e4bfa92c78a50319644ddc6e9f2054a207b44d09
Charles Srstka <support@charlessoft.com>
2023-01-21 18:21:15 -0500

    Add support for current directory to Foundation extension

--- Sources/PTYProcess_Foundation/PTYProcess+Foundation.swift
+++ Sources/PTYProcess_Foundation/PTYProcess+Foundation.swift
@@ -10,15 +10,30 @@ import System
 import PTYProcess
 
 extension PTYProcess {
-    public convenience init(executableURL: URL, arguments: [String] = [], environment: [String : String]? = nil) throws {
+    public convenience init(
+        executableURL: URL,
+        arguments: [String] = [],
+        environment: [String : String]? = nil,
+        currentDirectory: URL? = nil
+    ) throws {
         guard executableURL.isFileURL else {
             throw POSIXError(.EINVAL)
         }
 
         if #available(macOS 11.0, macCatalyst 14.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *) {
-            self.init(executablePath: FilePath(executableURL.path), arguments: arguments, environment: environment)
+            self.init(
+                executablePath: FilePath(executableURL.path),
+                arguments: arguments,
+                environment: environment,
+                currentDirectory: currentDirectory.map { FilePath($0.path) }
+            )
         } else {
-            self.init(executablePath: executableURL.path, arguments: arguments, environment: environment)
+            self.init(
+                executablePath: executableURL.path,
+                arguments: arguments,
+                environment: environment,
+                currentDirectory: currentDirectory?.path
+            )
         }
     }
 
